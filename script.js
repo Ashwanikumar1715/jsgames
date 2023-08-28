2569e44d724359fabdd41391bdde9b58a7b5b307
ashwanikumar <ashwanikumar01011997123@gmail.com>
2023-08-28 09:09:07 +0530

    first

---
+++ script.js
@@ -0,0 +1,26 @@
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = ()=>{
+  navbar.classList.add("show");
+  menuBtn.classList.add("hide");
+  body.classList.add("disabled");
+}
+cancelBtn.onclick = ()=>{
+  body.classList.remove("disabled");
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+}
+window.onscroll = ()=>{
+  this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}
+
+
+
+const toggleButton = document.getElementById('mode-toggle');
+const body1 = document.body;
+
+toggleButton.addEventListener('click', () => {
+  body1.classList.toggle('dark-mode');
+});
